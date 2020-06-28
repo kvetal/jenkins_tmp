3b2dcdb000509c06d8639630972f1403b2a43b22
KVetal <kvetal@mail.ru>
2020-06-28 14:35:22 +0500

    add sctips build_libpng

---
+++ scripts/build_libpng.groovy
@@ -0,0 +1,22 @@
+#!groovy
+//TEST JOB Build and install libpng
+properties([disableConcurrentBuilds()])
+
+pipeline{
+	agent {
+	label 'master'
+	}
+	options {
+		buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+		timestamps()
+	}
+	stages {
+		stage("Preparations"){
+			steps {
+				ws('~/ws') {
+					git 'git://git.code.sf.net/p/libpng/code'
+				}
+			}
+		}
+	}
+}
